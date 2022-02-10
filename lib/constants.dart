b375c76fea5eb588f0063784eac91521273fbcf3
Oluwaseyieniola <80404459+Oluwaseyieniola@users.noreply.github.com>
2022-02-10 06:34:50 -0800

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const KPrimaryColor = Color(0xFFFF7043);
+const KTextColor = Color(0xFF3C4046);
+const KBackgroundColor = Color(0XFFFFFFFF);
+const KTitleTextColor = Color(0xFF3C4046);
+const KShadowColor = Color(0XFFFFFFFF);
+const KPrimaryActiveShadowColor = Color(0xFFFF7043);
+const KTextLightColor = Color(0xFF3C4046);
+
+const KHeadingTextStyle = TextStyle(fontSize: 22, fontWeight: FontWeight.w600);
+
+const KSubTextStyle = TextStyle(fontSize: 16, color: KTextLightColor);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 18,
+  color: KTitleTextColor,
+  fontWeight: FontWeight.bold,
+);
